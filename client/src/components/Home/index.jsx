bde1fe41f032336009bb88150cc822c336d5bbfe
Gabriel Cavalcante <gabrielcalk@outlook.com>
2022-01-06 17:51:09 -0500

    added about project and about the author on the home page

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,10 +1,31 @@
 import RenderBannerHome from "../HomeBanner";
 import { Container } from "./style";
+import gabrielImg from '../../images/gabriel_image.jpg'
+import pcCodeImg from '../../images/pc_code.jpg'
 
 function RenderHomePage() {
     return (
         <Container>
             <RenderBannerHome/>
+            <section>
+                <div>
+                    <h2>About the Project</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro optio, nulla a vero mollitia laudantium libero obcaecati fugit rerum odit sapiente suscipit dicta voluptas aut expedita. Optio eius quibusdam tenetur?</p>
+                </div>
+                <div>
+                    <img src={pcCodeImg} alt="code"/>
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <img src={gabrielImg} alt="author"/>
+                </div>
+                <div>
+                    <h2>About the Author</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, porro ad fugiat doloribus minus sapiente et eos in vitae praesentium, pariatur facere. Qui harum ratione expedita cum et quod sint?</p>
+                </div>
+            </section>
         </Container>
     );
 }
--- client/src/components/Home/style.js
+++ client/src/components/Home/style.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-    
+    section{
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-top: 8rem;
+        
+        &:last-child{
+            margin-bottom: 8rem;
+        }
+
+        img{
+            width: 28rem;
+            height: 24rem;
+            border-radius: 0.5rem;
+        }
+
+        div{
+            margin: 0 auto;
+            width: 30rem;
+
+            h2, p{
+                text-align: center;
+            }
+        }
+    }
+
 `
