33117b45fbcc64efd89dfe038d065a0c53899a7c
Gabriel Cavalcante <gabrielcalk@outlook.com>
2022-01-12 16:19:04 -0500

    added the reference link to learn more later

--- client/src/utils/algorithm/search/binarySearch/animationsBinary.jsx
+++ client/src/utils/algorithm/search/binarySearch/animationsBinary.jsx
@@ -1,11 +1,12 @@
 function animationsBinary(result, setNumbers) {
-    setNumbers([])
+  setNumbers([]);
 
-    for (let i = 0; i < result.length; i++) {
-    //   setTimeout(() => {
-        setNumbers(oldState => [...oldState, result[i]])
-    //   }, i*100);
-    }
+  for (let i = 0; i < result.length; i++) {
+    // https://stackoverflow.com/questions/29537299/react-how-to-update-state-item1-in-state-using-setstate
+    setTimeout(() => {
+      setNumbers((oldState) => [...oldState, result[i]]);
+    }, i * 100);
+  }
 }
 
 export default animationsBinary;
