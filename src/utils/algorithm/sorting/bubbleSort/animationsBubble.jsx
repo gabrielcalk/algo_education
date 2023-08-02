44d7388c73c02cd32d451a29dedc8fc7c2fe87b7
Gabriel Cavalcante Causin <Gabriel.CavalcanteCausin@StoneX.com>
2023-08-02 09:54:06 -0400

    app

--- src/utils/algorithm/sorting/bubbleSort/animationsBubble.jsx
+++ src/utils/algorithm/sorting/bubbleSort/animationsBubble.jsx
@@ -1,27 +1,27 @@
-export default function animationBubble(animations, speed){
-    // doing one foor loop on the animation array (coming from ArraySort)
-    for(let i = 0; i < animations.length; i++){
-        const arrayBars = document.getElementsByClassName('array_bar');
-        const isColorChange = animations[i][2];
-    
-        if(isColorChange){
-          // Getting the barOne position and barTwo position from one specific animation (i)
-             const[barOneIdx , barTwoIdx] = animations[i];
-    
-          // If the Remainder of the position that I am is equal to 0, them turn to one color, else: change to another color
-             const color = i % 2 === 0 ? "#0ff" : "rgb(168, 58, 135)";
-          //  Changing the positions of the bars
-             setTimeout(()=>{
-              arrayBars[barOneIdx].style.backgroundColor=color;
-              arrayBars[barTwoIdx].style.backgroundColor=color;
-             }, i * speed);
-         }else{
-           setTimeout(()=>{
-            //  Changing the positions of the bars
-             const [barOneIdx , newHeight] = animations[i];
-             const barOneStyle = arrayBars[barOneIdx].style;
-             barOneStyle.height = `${newHeight}px`;
-           }, i * speed);
-         }
+export default function animationBubble(animations, speed) {
+  // doing one foor loop on the animation array (coming from ArraySort)
+  for (let i = 0; i < animations?.length; i++) {
+    const arrayBars = document.getElementsByClassName("array_bar");
+    const isColorChange = animations[i][2];
+
+    if (isColorChange) {
+      // Getting the barOne position and barTwo position from one specific animation (i)
+      const [barOneIdx, barTwoIdx] = animations[i];
+
+      // If the Remainder of the position that I am is equal to 0, them turn to one color, else: change to another color
+      const color = i % 2 === 0 ? "#0ff" : "rgb(168, 58, 135)";
+      //  Changing the positions of the bars
+      setTimeout(() => {
+        arrayBars[barOneIdx].style.backgroundColor = color;
+        arrayBars[barTwoIdx].style.backgroundColor = color;
+      }, i * speed);
+    } else {
+      setTimeout(() => {
+        //  Changing the positions of the bars
+        const [barOneIdx, newHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        barOneStyle.height = `${newHeight}px`;
+      }, i * speed);
     }
+  }
 }
--- src/utils/algorithm/sorting/bubbleSort/bubbleSort.jsx
+++ src/utils/algorithm/sorting/bubbleSort/bubbleSort.jsx
@@ -1,30 +1,28 @@
 export default function bubbleSort(array) {
   const animationArray = [];
 
-  const heightArrays = array
-  let sorted = false
-    
+  const heightArrays = array;
+  let sorted = false;
+
   // Bubble Sort Algo
   while (!sorted) {
-    sorted = true
-    for (let i = 0; i < heightArrays.length - 1; i++) {
-      animationArray.push([i,i+1,true]);
-      animationArray.push([i,i+1,true])
+    sorted = true;
+    for (let i = 0; i < heightArrays?.length - 1; i++) {
+      animationArray.push([i, i + 1, true]);
+      animationArray.push([i, i + 1, true]);
 
       if (heightArrays[i] > heightArrays[i + 1]) {
-
-        animationArray.push([i,heightArrays[i+1],false]);
-        animationArray.push([i+1,heightArrays[i],false]);
+        animationArray.push([i, heightArrays[i + 1], false]);
+        animationArray.push([i + 1, heightArrays[i], false]);
 
         // Changing positions
-        const actual = heightArrays[i]
-        heightArrays[i] = heightArrays[i + 1]
-        heightArrays[i + 1] = actual
+        const actual = heightArrays[i];
+        heightArrays[i] = heightArrays[i + 1];
+        heightArrays[i + 1] = actual;
 
-        sorted = false
+        sorted = false;
       }
     }
   }
-  return animationArray
+  return animationArray;
 }
-
